f78b8210388ec8c2d03e9a3cb098c19032fcab5c
Z.Chabot <chabzach95@gmail.com>
2024-07-21 16:04:34 -0400

    fix syntax error

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
 router.use("/items", likesRouter);
 router.use("*", (req, res) => {
-  return res.status(NOT_FOUND_STATUS).send(NOT_FOUND_MESSAGE);
+  res.status(NOT_FOUND_STATUS).send(NOT_FOUND_MESSAGE);
 });
 
 module.exports = router;

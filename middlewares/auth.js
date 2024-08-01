3d0ef8bc2c500c2211fa8f3973e9cd23752ca701
Z.Chabot <chabzach95@gmail.com>
2024-07-31 21:30:20 -0400

    finish adding error handling

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,13 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const {
-  UNAUTHORIZED_MESSAGE,
-  UNAUTHORIZED_STATUS,
-} = require("../utils/errors");
+const { UNAUTHORIZED_MESSAGE } = require("../utils/errors");
+const UnauthorizedError = require("../utils/Errors/UnauthorizedError");
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    res.status(UNAUTHORIZED_STATUS).send({ message: UNAUTHORIZED_MESSAGE });
+    next(new UnauthorizedError(UNAUTHORIZED_MESSAGE));
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -18,9 +16,7 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res
-      .status(UNAUTHORIZED_STATUS)
-      .send({ message: UNAUTHORIZED_MESSAGE });
+    next(new UnauthorizedError(UNAUTHORIZED_MESSAGE));
   }
 
   req.user = payload;
--- middlewares/checkOwner.js
+++ middlewares/checkOwner.js
@@ -1,14 +1,12 @@
 const ClothingItem = require("../models/clothingItem");
 const {
   BAD_REQUEST_MESSAGE,
-  BAD_REQUEST_STATUS,
   NOT_FOUND_MESSAGE,
-  NOT_FOUND_STATUS,
-  DEFAULT_MESSAGE,
-  DEFAULT_STATUS,
   FORBIDDEN_MESSAGE,
-  FORBIDDEN_STATUS,
 } = require("../utils/errors");
+const BadRequestError = require("../utils/Errors/BadRequestError");
+const NotFoundError = require("../utils/Errors/NotFoundError");
+const ForbiddenError = require("../utils/Errors/ForbiddenError");
 
 const checkOwner = (req, res, next) => {
   const { itemId } = req.params;
@@ -20,21 +18,17 @@ const checkOwner = (req, res, next) => {
       if (itemOwner === ownerId) {
         return next();
       }
-      return res.status(FORBIDDEN_STATUS).send({ message: FORBIDDEN_MESSAGE });
+      next(new ForbiddenError(FORBIDDEN_MESSAGE));
     })
     .catch((err) => {
       console.error(err);
       if (err.name === "DocumentNotFoundError") {
-        return res
-          .status(NOT_FOUND_STATUS)
-          .send({ message: NOT_FOUND_MESSAGE });
+        next(new NotFoundError(NOT_FOUND_MESSAGE));
       }
       if (err.name === "CastError") {
-        return res
-          .status(BAD_REQUEST_STATUS)
-          .send({ message: BAD_REQUEST_MESSAGE });
+        next(new BadRequestError(BAD_REQUEST_MESSAGE));
       }
-      return res.status(DEFAULT_STATUS).send({ message: DEFAULT_MESSAGE });
+      next();
     });
 };
 
